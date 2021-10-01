ece858583bfa8f298e7dc7416d2054792208b429
dev-david-alves <davidalves9090@gmail.com>
2021-10-01 20:08:33 -0300

    Adicionada Funções de Contagem.

--- AtividadeEstressados/estressados.cpp
+++ AtividadeEstressados/estressados.cpp
@@ -127,6 +127,112 @@ bool procurar_melhor_se(vector<int> stress) {
     return true;
 }
 
+// Funções de Contagem
+
+bool calcular_stress_medio(vector<int> stress) {
+    double stress_medio = 0;
+
+    int sz = (int) stress.size();
+
+    for(int i = 0; i < sz; i++) {
+        stress_medio += abs(stress[i]);
+    }
+
+    stress_medio /= (double) sz;
+
+    cout << "A media de stress da fila e: " << stress_medio << "\n";
+
+    return true;
+}
+
+bool mais_homens_ou_mulheres(vector<int> stress) {
+    int homens = 0;
+    int mulheres = 0;
+
+    int sz = (int) stress.size();
+
+    for(int i = 0; i < sz; i++) {
+        if(stress[i] > 0) {
+            homens++;
+        } else {
+            mulheres++;
+        }
+    }
+
+    if(homens > mulheres) {
+        cout << "Ha mais homens do que mulheres na fila." << "\n";
+    } else if(homens < mulheres) {
+        cout << "Ha mais mulheres do que homens na fila." << "\n";
+    } else {
+        cout << "Quantidade de homens e mulheres na fila igual." << "\n";
+    }
+
+    return true;
+}
+
+bool qual_metade_e_mais_estressada(vector<int> stress) {
+    int primeira = 0;
+    int segunda = 0;
+
+    int sz = (int) stress.size();
+
+    for(int i = 0; i < sz; i++) {
+        if(i < sz / 2) {
+            primeira += abs(stress[i]);
+        } else {
+            segunda += abs(stress[i]);
+        }
+    }
+
+    if(primeira > segunda) {
+        cout << "O nivel de stress da primeira parte e maior que o da segunda parte da fila." << "\n";
+    } else if(primeira < segunda) {
+        cout << "O nivel de stress da segunda parte e maior que o da primeira parte da fila." << "\n";
+    } else {
+        cout << "Nivel de stress da duas partes da fila e igual." << "\n";
+    }
+
+    return true;
+}
+
+bool homens_sao_mais_estressados_que_mulheres(vector<int> stress) {
+    double stress_homens = 0;
+    double stress_mulheres = 0;
+
+    double homens_qntd = 0;
+    double mulheres_qntd = 0;
+
+    int sz = (int) stress.size();
+
+    for(int i = 0; i < sz; i++) {
+        if(stress[i] > 0) {
+            stress_homens += abs(stress[i]);
+            homens_qntd++;
+        } else {
+            stress_mulheres += abs(stress[i]);
+            mulheres_qntd++;
+        }
+    }
+
+    if(homens_qntd > 0) {
+        stress_homens /= homens_qntd;
+    } 
+    
+    if(mulheres_qntd > 0) {
+        stress_mulheres /= mulheres_qntd;
+    }
+
+    if(stress_homens > stress_mulheres) {
+        cout << "O nivel de stress dos homens e maior que o das mulheres na fila." << "\n";
+    } else if(stress_homens < stress_mulheres) {
+        cout << "O nivel de stress das mulheres e maior que o dos homens da fila." << "\n";
+    } else {
+        cout << "O nivel de stress dos homens e das mulheres e igual." << "\n";
+    }
+
+    return true;
+}
+
 // Selecionar função do item
 
 bool selecionar_item(char item, vector<int> stress) {
@@ -155,10 +261,13 @@ bool selecionar_item(char item, vector<int> stress) {
         break;
     case 'e':
         return procurar_menor_maior(stress, 'm');
+        break;
     case 'f':
         return procurar_menor_maior(stress, 'M');
+        break;
     case 'g':
         return procurar_menor_pos(stress, 0);
+        break;
     case 'h':
         cout << "Digite a posicao que deseja comecar a busca: ";
         cin >> pos;
@@ -172,6 +281,19 @@ bool selecionar_item(char item, vector<int> stress) {
         break;
     case 'i':
         return procurar_melhor_se(stress);
+        break;
+    case 'j':
+        return calcular_stress_medio(stress);
+        break;
+    case 'k':
+        return mais_homens_ou_mulheres(stress);
+        break;
+    case 'l':
+        return qual_metade_e_mais_estressada(stress);
+        break;
+    case 'm':
+        return homens_sao_mais_estressados_que_mulheres(stress);
+        break;
     default:
         break;
     }
@@ -184,8 +306,8 @@ int main() {
     char resposta;
     char item;
 
-    vector<char> items{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-    vector<int> stress{-11, 1, 39, -11, 2, 5, -99, 33, 22, -34};
+    vector<char> items{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+    vector<int> stress{-11, 99, 39, -99, 85, 57, -99, -33, -22, -34};
 
     cout << "Deseja coletar informacoes sobre a fila? Digite a letra 's' ou 'S' para continuar." << "\n";
     cin >> resposta;
@@ -206,6 +328,12 @@ int main() {
             cout << "(g) Qual a posicao do menor valor da lista?" << "\n";
             cout << "(h) Qual a posicao do menor valor da lista depois da posicao P?" << "\n";
             cout << "(i) Qual a posicao do HOMEM mais calmo? (menor valor maior que 0)" << "\n";
+
+            cout << "Contagem ------------->" << "\n";
+            cout << "(j) Qual a media de stress da fila? (abs)" << "\n";
+            cout << "(k) Na fila existem mais homens ou mulheres?" << "\n";
+            cout << "(l) O nivel de stress somado de todas as pessoas da primeira metade e maior que o nivel de stress somado das pessoas da segunda metade da fila? (abs)" << "\n";
+            cout << "(m) A media do stress dos homens e maior que a das mulheres?" << "\n";
 
             cout << "Item: ";
             cin >> item;
