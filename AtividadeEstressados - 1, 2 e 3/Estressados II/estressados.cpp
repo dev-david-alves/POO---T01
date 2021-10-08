5f5c4e87d194771b567834fd5f6312311fa95101
dev-david-alves <davidalves9090@gmail.com>
2021-10-07 23:01:19 -0300

    Testador adicionado.

--- AtividadeEstressados - 1, 2 e 3/Estressados II/estressados.cpp
+++ AtividadeEstressados - 1, 2 e 3/Estressados II/estressados.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <ostream>
+#include <algorithm>
+
+#include "../testador.hpp"
 
 int main() {
 
+    testar(5, 6);
+    testar(std::string("banana"), std::string("banana"));
+    testar(std::vector<int> {1, 2, 3}, std::vector<int> {1, 2, 3});
+    std::cout << std::vector<int> {1, 2, 3, 4, 5} << "\n";
+
     return 0;
 }
+
