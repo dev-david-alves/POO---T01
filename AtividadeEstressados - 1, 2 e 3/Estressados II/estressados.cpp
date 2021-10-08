8eb792dbebae2d978209f5455c2c00bd042dc439
dev-david-alves <davidalves9090@gmail.com>
2021-10-08 00:10:20 -0300

    Bloco II - Função de Acesso adicionadas.

--- AtividadeEstressados - 1, 2 e 3/Estressados II/estressados.cpp
+++ AtividadeEstressados - 1, 2 e 3/Estressados II/estressados.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
 #include <ostream>
 #include <algorithm>
+#include <time.h>
 
 #include "../testador.hpp"
 
@@ -61,19 +63,136 @@ std::vector<int> pegar_mulheres_calmas(const std::vector<int>& v) {
 }
 
 void teste_pegar_mulheres_calmas() {
-    std::cout << "pegar_mulheres_calmas\n";
+    std::cout << "teste_pegar_mulheres_calmas\n";
     testar(pegar_mulheres_calmas({-1, -22, 30}), {0});
     testar(pegar_mulheres_calmas({66, -2, -32, 4, 5}), {1});
     testar(pegar_mulheres_calmas({3, -52, -8, 12}), {1, 2});
 }
 
+// Funções de Acesso
+
+std::vector<int> inverter_com_copia(const std::vector<int>& v) {
+    std::vector<int> invertido;
+
+    for(int i = (int) v.size() -1; i >= 0; i--)
+        invertido.push_back(v[i]);
+
+    return invertido;
+}
+
+void teste_inverter_com_copia() {
+    std::cout << "teste_inverter_com_copia\n";
+    testar(inverter_com_copia({-1, -22, 30}), {30, -22, -1});
+    testar(inverter_com_copia({66, -2, -32, 4, 5}), {5, 4, -32, -2, 66});
+    testar(inverter_com_copia({3, -52, -8, 12}), {3, -52, 12, -8});
+}
+
+void inverter_inplace(std::vector<int>& v) {
+    for(int i = 0; i < (int) v.size() / 2; i++)
+        std::swap(v[i], v[((int) v.size()) - i - 1]);
+}
+
+void teste_inverter_inplace() {
+    std::cout << "teste_inverter_inplace\n";
+
+    std::vector<int> v1 {-1, -22, 30};
+    std::vector<int> v2 {66, -2, -32, 4, 5};
+    std::vector<int> v3 {3, -52, -8, 12};
+
+    inverter_inplace(v1);
+    inverter_inplace(v2);
+    inverter_inplace(v3);
+
+    testar(v1, {30, -22, -1});
+    testar(v2, {5, 4, -32, -2, 66});
+    testar(v3, {3, -52, 12, -8});
+}
+
+//retorna aleatoriamente um elemento do vetor
+int sortear(const std::vector<int>& v) {
+    srand(time(NULL));
+    int size { (int) v.size() };
+    int indice { rand() % size};
+    return v[indice];
+}
+
+void teste_sortear() {
+    std::cout << "teste_inverter_inplace\n";
+
+    std::cout << sortear({-1, -34, 2, -4, 55}) << "\n";
+    std::cout << sortear({44, -34, 22, 32, 5}) << "\n";
+    std::cout << sortear({31, -34, 42, 44, 89}) << "\n";
+}
+
+//embaralha o vetor original
+void embaralhar(std::vector<int>& v) {
+    srand(time(NULL));
+    int size { (int) v.size() };
+    int i_1 {};
+    int i_2 {};
+
+    for(int i = 0; i < (int) v.size() / 2; i++) {
+        i_1 = rand() % size;
+        i_2 = rand() % size;
+
+        std::swap(v[i_1], v[i_2]);
+    }
+}
+
+void teste_embaralhar() {
+    std::cout << "teste_embaralhar\n";
+
+    std::vector<int> v1 {-1, -22, 30};
+    std::vector<int> v2 {66, -2, -32, 4, 5};
+    std::vector<int> v3 {3, -52, -8, 12};
+
+    std::cout << v1 << " -> ";
+    embaralhar(v1);
+    std::cout << v1 << "\n";
+
+    std::cout << v2 << " -> ";
+    embaralhar(v2);
+    std::cout << v2 << "\n";
+
+    std::cout << v3 << " -> ";
+    embaralhar(v3);
+    std::cout << v3 << "\n";
+}
+
+void ordenar(std::vector<int>& v) {
+    std::sort(v.begin(), v.end());
+}
+
+void teste_ordenar() {
+    std::cout << "teste_ordenar\n";
+
+    std::vector<int> v1 {-1, -22, 30};
+    std::vector<int> v2 {66, -2, -32, 4, 5};
+    std::vector<int> v3 {3, -52, -8, 12};
+
+    std::cout << v1 << " -> ";
+    ordenar(v1);
+    std::cout << v1 << "\n";
+
+    std::cout << v2 << " -> ";
+    ordenar(v2);
+    std::cout << v2 << "\n";
+
+    std::cout << v3 << " -> ";
+    ordenar(v3);
+    std::cout << v3 << "\n";
+}
 
 int main() {
-    teste_clone();
-    teste_pegar_homens();
-    teste_pegar_calmos();
-    teste_pegar_mulheres_calmas();
-
+    // teste_clone();
+    // teste_pegar_homens();
+    // teste_pegar_calmos();
+    // teste_pegar_mulheres_calmas();
+    // teste_inverter_com_copia();
+    // teste_inverter_inplace();
+    // teste_sortear();
+    // teste_embaralhar();
+    // teste_ordenar();
 
     return 0;
 }
