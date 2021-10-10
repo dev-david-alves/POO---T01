419046feaa87b9f45cfc8b15ce8c2f0273904b4c
dev-david-alves <davidalves9090@gmail.com>
2021-10-10 17:33:47 -0300

    Background, chão e animação do player adicionados.

--- Lobo e Coelho na floresta/main.cpp
+++ Lobo e Coelho na floresta/main.cpp
@@ -1,5 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include<windows.h>
+
+sf::Texture loadTexture(std::string path) {
+    sf::Texture texture;
+    if(!texture.loadFromFile(path)) {
+        std::cout << "Error loading texture\n";
+        exit(1);
+    }
+
+    return texture;
+}
 
 void setSize(sf::Sprite& sprite, int width, int height) {
     auto dim = sprite.getLocalBounds();
@@ -12,13 +23,41 @@ struct Entity {
     int step {0};
     sf::Sprite sprite;
 
+    bool is_idle {true};
+    bool is_mirrored {false};
+
+    std::vector<sf::Texture> idle_frames { loadTexture("./images/Idle (1).png"), loadTexture("./images/Idle (2).png"), loadTexture("./images/Idle (3).png"), loadTexture("./images/Idle (4).png"), loadTexture("./images/Idle (5).png")};
+
+    std::vector<sf::Texture> walk_frames { loadTexture("./images/Walk (1).png"), loadTexture("./images/Walk (2).png"), loadTexture("./images/Walk (3).png"), loadTexture("./images/Walk (4).png"), loadTexture("./images/Walk (5).png"), loadTexture("./images/Walk (6).png"), loadTexture("./images/Walk (7).png"), loadTexture("./images/Walk (8).png"), loadTexture("./images/Walk (9).png"), loadTexture("./images/Walk (10).png") };
+
+    int idle_frame_count {0};
+    int walk_frame_count {0};
+
     Entity (int x, int y, int step, sf::Texture& texture):
         x {x}, y {y}, step {step}, sprite(texture) {
     }
 
     void draw(sf::RenderWindow& window) {
-        this->sprite.setPosition(x * step, y * step);
-        setSize(this->sprite, step, step);
+        if(is_idle) {
+            walk_frame_count = 0;
+
+            this->sprite.setTexture(this->idle_frames[idle_frame_count]);
+            idle_frame_count = (idle_frame_count + 1) % idle_frames.size();
+        } else {
+            idle_frame_count = 0;
+
+            this->sprite.setTexture(this->walk_frames[walk_frame_count]);
+            walk_frame_count = (walk_frame_count + 1) % walk_frames.size();
+        }
+
+        this->sprite.setPosition(x * step / 5, y * step / 5);
+        if(is_mirrored) {
+            setSize(this->sprite, -step, step);
+            this->sprite.setPosition(x * step / 5 + step, y * step / 5);
+        } else {
+            setSize(this->sprite, step, step);
+        }
+
         window.draw(this->sprite);
     }
 };
@@ -37,59 +76,76 @@ struct Board {
         setSize(this->sprite, nc * step, nl * step);
         this->sprite.setPosition(0, 0);
 
-        this->rect.setSize(sf::Vector2f(step, step));
-        this->rect.setFillColor(sf::Color::Transparent);
+        // this->rect.setSize(sf::Vector2f(step, step));
+        // this->rect.setFillColor(sf::Color::Transparent);
 
-        this->rect.setOutlineColor(sf::Color::Black);
-        this->rect.setOutlineThickness(2);
+        // this->rect.setOutlineColor(sf::Color::Black);
+        // this->rect.setOutlineThickness(2);
     }
 
     void draw(sf::RenderWindow& window) {
         window.draw(this->sprite);
 
-        for(int i = 0; i < nc; i++) {
-            for(int j = 0; j < nl; j++) {
-                this->rect.setPosition(i * step, j * step);
-                window.draw(this->rect);
-            }
-        }
+        // for(int i = 0; i < nc; i++) {
+        //     for(int j = 0; j < nl; j++) {
+        //         this->rect.setPosition(i * step, j * step);
+        //         window.draw(this->rect);
+        //     }
+        // }
     }
 };
 
 void moveEntity(sf::Keyboard::Key key, Entity& entity, std::vector<sf::Keyboard::Key> move_keys) {
-    if(key == move_keys[0])
+    if(key == move_keys[0]) {
         entity.x--;
-    else if(key == move_keys[1])
+        entity.is_idle = false;
+
+        if(!entity.is_mirrored) {
+            entity.is_mirrored = true;
+        }
+    } else if(key == move_keys[1]) {
         entity.y--;
-    else if(key == move_keys[2])
+        entity.is_idle = false;
+    } else if(key == move_keys[2]) {
         entity.x++;
-    else if(key == move_keys[3])
+        entity.is_idle = false;
+
+        if(entity.is_mirrored) {
+            entity.is_mirrored = false;
+        }
+    } else if(key == move_keys[3]) {
         entity.y++;
-}
-
-sf::Texture loadTexture(std::string path) {
-    sf::Texture texture;
-    if(!texture.loadFromFile(path)) {
-        std::cout << "Error loading texture\n";
-        exit(1);
+        entity.is_idle = false;
+    } else {
+        entity.is_idle = true;
     }
-
-    return texture;
 }
 
 int main() {
 
-    sf::Texture place { loadTexture("./images/place.jpg") };
-    sf::Texture wolf_tex { loadTexture("./images/wolf.png") };
-    sf::Texture rabbit_tex { loadTexture("./images/rabbit.png") };
+    sf::Texture background_tex { loadTexture("./images/background.png") };
+    sf::Texture ground_tex { loadTexture("./images/ground.png") };
+    sf::Texture zombie_tex { loadTexture("./images/Walk (1).png") };
+    // sf::Texture rabbit_tex { loadTexture("./images/rabbit.png") };
 
     const int STEP {100};
 
-    Entity wolf(1, 1, STEP, wolf_tex);
-    Entity rabbit(3, 3, STEP, rabbit_tex);
-    Board board(7, 5, STEP, place);
+    Entity zombie(1, 12, STEP, zombie_tex);
+    // Entity rabbit(3, 3, STEP, rabbit_tex);
+    Board board(7, 5, STEP, background_tex);
+
+    sf::Sprite ground_sp;
+    ground_sp.setTexture(ground_tex);
 
     sf::RenderWindow window(sf::VideoMode(board.nc * STEP, board.nl * STEP), "O Lobo e o Coelho na Floresta!", sf::Style::Close);
+
+    char ground_matrix[5][7] = {
+        {'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+        {'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+        {'e', 'e', 'e', 'e', 'e', 'e', 'e'},
+        {'e', 'e', 'e', 'e', 'e', 'g', 'g'},
+        {'g', 'g', 'g', 'e', 'g', 'g', 'g'},
+    };
 
     while (window.isOpen()) {
         sf::Event event;
@@ -97,17 +153,31 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             } else if(event.type == sf::Event::KeyPressed) {
-                moveEntity(event.key.code, wolf, {sf::Keyboard::Left, sf::Keyboard::Up, sf::Keyboard::Right, sf::Keyboard::Down});
-
-                moveEntity(event.key.code, rabbit, {sf::Keyboard::Left, sf::Keyboard::Up, sf::Keyboard::Right, sf::Keyboard::Down});
+                moveEntity(event.key.code, zombie, {sf::Keyboard::Left, sf::Keyboard::Up, sf::Keyboard::Right, sf::Keyboard::Down});
+            } else if(event.type == sf::Event::KeyReleased) {
+                zombie.is_idle = true;
             }
         }
 
         window.clear();
+
         board.draw(window);
-        wolf.draw(window);
-        rabbit.draw(window);
+        zombie.draw(window);
+        // rabbit.draw(window);
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 7; j++) {
+                if(ground_matrix[i][j] != 'e') {
+                    ground_sp.setPosition(j * STEP, i * STEP);
+                    window.draw(ground_sp);
+                }
+            }
+        }
+
         window.display();
+
+
+        Sleep(80);
     }
 
     return 0;
