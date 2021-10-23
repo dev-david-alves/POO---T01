65a7f43c01f07bed9cb7d28d75675604edb5e376
dev-david-alves <davidalves9090@gmail.com>
2021-10-23 20:46:33 -0300

    Hit, Misses, E input handler Adicionados.

--- SFML 02 - Jogo da Digitacao/main.cpp
+++ SFML 02 - Jogo da Digitacao/main.cpp
@@ -53,13 +53,11 @@ struct Bubble {
 struct Board {
     sf::RenderWindow& window;
     std::vector<Bubble> bubbles;
+    Pencil pencil;
     int hits {0};
     int misses {0};
 
-    Board(sf::RenderWindow& window) : window(window) {
-        bubbles.push_back(Bubble(100, 100, 'A', 1));
-        bubbles.push_back(Bubble(200, 100, 'B', 2));
-        bubbles.push_back(Bubble(300, 100, 'C', 3));
+    Board(sf::RenderWindow& window) : window(window), pencil(window) {
     }
 
     void add_new_bubble() {
@@ -77,6 +75,41 @@ struct Board {
         for(Bubble& bubble: bubbles) {
             bubble.update();
         }
+
+        this->mark_outside_bubbles();
+        this->remove_dead_bubbles();
+    }
+
+    void mark_outside_bubbles() {
+        for(Bubble& bubble: bubbles) {
+            if(bubble.y + 2 * Bubble::radius > (int) this->window.getSize().y) {
+                if(bubble.alive) {
+                    bubble.alive = false;
+                    this->misses++;
+                }
+            }
+        }
+    }
+
+    void mark_by_hit(char letter) {
+        for(Bubble& bubble: bubbles) {
+            if(bubble.letter == letter) {
+                bubble.alive = false;
+                this->hits++;
+                break;
+            }
+        }
+    }
+
+    void remove_dead_bubbles() {
+        std::vector<Bubble> alive;
+
+        for(Bubble& bubble: bubbles) {
+            if(bubble.alive)
+                alive.push_back(bubble);
+        }
+
+        this->bubbles = alive;
     }
 
     bool check_new_bubble() {
@@ -93,6 +126,9 @@ struct Board {
     }
 
     void draw() {
+        pencil.write("Hits: " + std::to_string(this->hits) + "\t\tMisses: " + std::to_string(this->misses), 10, 10, 20, sf::Color::White);
+        pencil.write("Size: " + std::to_string(bubbles.size()), 10, 30, 20, sf::Color::White);
+
         for(Bubble& bubble: bubbles) {
             bubble.draw(window);
         }
@@ -113,6 +149,10 @@ struct Game {
         while(window.pollEvent(event)) {
             if(event.type == sf::Event::Closed) {
                 window.close();
+            } else if(event.type == sf::Event::TextEntered) {
+                char code = static_cast<char>(event.text.unicode);
+                code = toupper(code);
+                board.mark_by_hit(code);
             }
         }
     }
@@ -133,7 +173,6 @@ struct Game {
 };
 
 int main() {
-
     Game game;
     game.main_loop();
  
