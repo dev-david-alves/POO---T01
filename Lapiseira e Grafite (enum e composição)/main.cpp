4be4e719d2938d51a31eebcd14fc22bbd6304be7
dev-david-alves <davidalves9090@gmail.com>
2021-10-24 12:22:35 -0300

    Inserir e remover grafite concluídos.

--- Lapiseira e Grafite (enum e composição)/main.cpp
+++ Lapiseira e Grafite (enum e composição)/main.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
+#include <algorithm>
+
+struct Grafite {
+    float calibre;
+    std::string dureza;
+    int tamanho;
+
+    Grafite(float calibre = 0, std::string dureza = "", int tamanho = 0) : calibre(calibre), dureza(dureza), tamanho(tamanho) {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Grafite& grafite) {
+        os << "Calibre: " << grafite.calibre << " mm, ";
+        os << "Dureza: " << grafite.dureza << ", ";
+        os << "Tamanho: " << grafite.tamanho << " mm\n";
+
+        return os;
+    }
+};
+
+struct Lapiseira {
+    float calibre;
+    Grafite* grafite;
+
+    Lapiseira(float calibre, Grafite* grafite) : calibre(calibre), grafite(grafite) {
+    }
+
+    bool inserirGrafite(Grafite *grafite) {
+        if(this->grafite != nullptr) {
+            std::cout << "Já tem grafite.\n";
+            return false;
+        }
+
+        if(grafite->calibre != this->calibre) {
+            std::cout << "Calibre imcompatível.\n";
+            return false;
+        }
+
+        this->grafite = grafite;
+        return true;
+    }
+
+    Grafite* removerGrafite() {
+        if(this->grafite == nullptr) {
+            std::cout << "Já está sem grafite.\n";
+            return nullptr;
+        }
+
+        return std::exchange(this->grafite, nullptr);
+    }
+};
+
+
+
 
 int main() {
 
-    std::cout << "Iniciando Lapiseira e Grafite.\n";
+    Grafite grafite(1.0, "HB", 10);
+    Lapiseira lapiseira(0.1, &grafite);
+
+    lapiseira.grafite->tamanho -= 5;
+
+    std::cout << lapiseira.grafite->tamanho;
 
     return 0;
 }
